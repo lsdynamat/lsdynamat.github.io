8776b694f6d77155aea4d346ddfd6c855f775460
lsdynamat <lsdynamat@gmail.com>
2025-12-26 16:19:14 +0100

    Update index.js

--- assets/js/generators/index.js
+++ assets/js/generators/index.js
@@ -1,3 +1,4 @@
+// assets/js/generators/index.js
 import * as mat015 from "./mat015_johnson_cook.js";
 import * as mat027 from "./mat027_mooney_rivlin.js";
 import * as mat084 from "./mat084_winfrith.js";
@@ -7,9 +8,17 @@ export const GENERATORS = {
   mat015_johnson_cook: mat015,
   mat027_mooney_rivlin: mat027,
   mat084_winfrith: mat084,
-  mat159_cscm: mat159
+  mat159_cscm: mat159,
 };
 
-export function getGenerator(key){
+export const GENERATOR_KEYS = Object.keys(GENERATORS).sort((a, b) => {
+  // sort by numeric id if present: mat159_... > 159
+  const na = Number((a.match(/^mat(\d+)/) || [])[1] || 999999);
+  const nb = Number((b.match(/^mat(\d+)/) || [])[1] || 999999);
+  if (na !== nb) return na - nb;
+  return a.localeCompare(b);
+});
+
+export function getGenerator(key) {
   return GENERATORS[key] || null;
 }
