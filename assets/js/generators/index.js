06d3dd72cfe09231bd5e61bcbc94accfd3dff3a9
lsdynamat <lsdynamat@gmail.com>
2025-12-29 17:12:26 +0100

    Update index.js

--- assets/js/generators/index.js
+++ assets/js/generators/index.js
@@ -4,13 +4,14 @@ import * as mat027 from "./mat027_mooney_rivlin.js";
 import * as mat084 from "./mat084_winfrith.js";
 import * as mat159 from "./mat159_cscm.js";
 import * as mat072 from "./mat072_kcc_rel3.js";
-
+import * as mat273_cdp from "./mat273_cdp.js";
 export const GENERATORS = {
   mat015_johnson_cook: mat015,
   mat027_mooney_rivlin: mat027,
   mat084_winfrith: mat084,
   mat159_cscm: mat159,
   mat072_kcc_rel3: mat072,
+  mat273_cdp: mat273_cdp,
 };
 
 // IMPORTANT: for dropdown order + stable sorting
