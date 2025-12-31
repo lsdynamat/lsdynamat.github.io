226d3dd54a0b154f59c2056e25fb8ed077d59953
lsdynamat <lsdynamat@gmail.com>
2025-12-31 09:35:34 +0100

    Update index.js

--- assets/js/generators/index.js
+++ assets/js/generators/index.js
@@ -2,6 +2,7 @@
 
 import * as mat003 from "./mat003_plastic_kinematic.js";
 import * as mat015 from "./mat015_johnson_cook.js";
+import * as mat024 from "./mat024_piecewise_linear_plasticity.js";
 import * as mat027 from "./mat027_mooney_rivlin.js";
 import * as mat072 from "./mat072_kcc_rel3.js";
 import * as mat084 from "./mat084_winfrith.js";
@@ -11,6 +12,7 @@ import * as mat273 from "./mat273_cdp.js";
 export const GENERATORS = {
   mat003_plastic_kinematic: mat003,
   mat015_johnson_cook: mat015,
+  mat024_piecewise_linear_plasticity: mat024,
   mat027_mooney_rivlin: mat027,
   mat072_kcc_rel3: mat072,
   mat084_winfrith: mat084,
